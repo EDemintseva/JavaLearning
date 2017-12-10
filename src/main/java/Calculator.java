35e0222e8b059ee141736953118350daa8ea6229
Ekaterina <katrindemine@gmail.com>
2017-12-10 21:00:27 +0300

    Задание 1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * @author Ekaterina Demintseva
+ * В классе Calculator реализован сложение дробных чисел, которые пользователь вводит в консоль.
+ */
+
+public class Calculator {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        //Ввод первого слагаемого
+        System.out.println("Enter the first number: ");
+        float firstNumber = scanner.nextFloat();
+        //Ввод второго слагаемого
+        System.out.println("Enter the second number: ");
+        float secondNumber = scanner.nextFloat();
+        //Вывод результата
+        float result = firstNumber + secondNumber;
+        System.out.printf("Sum is: %.4f", result);
+
+        scanner.close();
+    }
+}
